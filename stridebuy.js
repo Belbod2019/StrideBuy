6ae2b5145ab37b48de80d9acd2909220b50f29fa
Plausible <67469355+Belbod2019@users.noreply.github.com>
2024-08-27 23:40:42 +0100

    Add files via upload

---
+++ stridebuy.js
@@ -0,0 +1,6 @@
+let cart = [];
+
+function addToCart(productName, productPrice) {
+    cart.push({ name: productName, price: productPrice });
+    alert(`${productName} has been added to your cart.`);
+}
